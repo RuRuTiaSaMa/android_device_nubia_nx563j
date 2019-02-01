4f7de46ca5cee98770867d9cdfca90661a9c7033
RuRuTiaSaMa <1009087450@qq.com>
2019-02-01 22:09:22 +0800

    clean up livedisplay
    Signed-off-by: RuRuTiaSaMa <1009087450@qq.com>

--- device.mk
+++ device.mk
@@ -42,11 +42,6 @@ TARGET_SCREEN_WIDTH := 1080
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 
-# Display configs
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/display/calib.cfg:$(TARGET_COPY_OUT_VENDOR)/etc/calib.cfg \
-    $(LOCAL_PATH)/display/qdcm_calib_data_jdi_r63452_1080p_5p5_mipi_cmd_panel.xml:$(TARGET_COPY_OUT_VENDOR)/etc/qdcm_calib_data_jdi_r63452_1080p_5p5_mipi_cmd_panel.xml
-
 # Camera
 PRODUCT_PACKAGES += \
     camera.msm8998
