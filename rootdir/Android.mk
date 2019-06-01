917d34efc1816f28d50990b048c2eb950dea759d
RuRuTiaSaMa <1009087450@qq.com>
2019-06-01 11:05:31 +0800

    move all *.rc to vendor
    Signed-off-by: RuRuTiaSaMa <1009087450@qq.com>

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -5,6 +5,6 @@ include $(CLEAR_VARS)
 LOCAL_MODULE       := init.nubia.touch.lcd.rc
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := root/init.nubia.touch.lcd.rc
-LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+LOCAL_SRC_FILES    := vendor/etc/init/hw/init.nubia.touch.lcd.rc
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
 include $(BUILD_PREBUILT)
